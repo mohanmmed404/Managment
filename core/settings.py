1e932e04f2204956464a5231b018dc5473abc787
mohanmmed404 <f.mj620@gmail.com>
2025-01-30 10:47:43 +0300

    last jaber 2
    last jaber 2

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',  # Enable the inner app
-    'customers'
+    'customers',
+    'django_ftpserver'
 ]
 
 MIDDLEWARE = [
